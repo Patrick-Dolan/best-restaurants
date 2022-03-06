c06b5ef6346ea7956d0da7ed4d41f5a26468f52c
Patrick Dolan <dolanp1992@gmail.com>
2022-03-06 15:37:28 -0800

    change from template to best restaurants project
    Co-authored-by: Branden Clauson <clauson.branden@gmail.com>

--- BestRestaurants/Startup.cs
+++ BestRestaurants/Startup.cs
@@ -5,7 +5,7 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
-namespace ProjectName
+namespace BestRestaurants
 {
   public class Startup
   {
