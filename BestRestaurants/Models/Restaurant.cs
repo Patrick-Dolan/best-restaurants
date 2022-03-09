d5d1c3182912ec9a3d8c4905eccb6285ad443d4f
Patrick Dolan <dolanp1992@gmail.com>
2022-03-08 20:17:49 -0800

    add properties from database
    Co-authored-by: Branden Clauson <clauson.branden@gmail.com>
    Co-authored-by: Marni Sucher <suchermarni@gmail.com>

--- BestRestaurants/Models/Restaurant.cs
+++ BestRestaurants/Models/Restaurant.cs
@@ -2,6 +2,8 @@ namespace BestRestaurants.Models
 {
   public class Restaurant
   {
-    
+    public int RestaurantId { get; set; }
+    public string Name { get; set; }
+    public string Description { get; set; }
   }
-}
+}   
