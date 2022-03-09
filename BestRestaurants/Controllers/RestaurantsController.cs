1a7922c8863b42aa2d972ec1509f6604e1cb83eb
Patrick Dolan <dolanp1992@gmail.com>
2022-03-08 20:18:23 -0800

    add controller and index page for main view
    Co-authored-by: Branden Clauson <clauson.branden@gmail.com>
    Co-authored-by: Marni Sucher <suchermarni@gmail.com>

---
+++ BestRestaurants/Controllers/RestaurantsController.cs
@@ -0,0 +1,36 @@
+using Microsoft.AspNetCore.Mvc;
+using BestRestaurants.Models;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace BestRestaurants.Controllers
+{
+  public class RestaurantsController : Controller
+  {
+    private readonly BestRestaurantsContext _db;
+
+    public RestaurantsController(BestRestaurantsContext db)
+    {
+      _db = db;
+    }
+
+    public ActionResult Index()
+    {
+      List<Restaurant> model = _db.Restaurants.ToList();
+      return View(model);
+    }
+
+    public ActionResult Create()
+    {
+      return View();
+    }
+
+    [HttpPost]
+    public ActionResult Create(Restaurant restaurant)
+    {
+      _db.Restaurants.Add(restaurant);
+      _db.SaveChanges();
+      return RedirectToAction("Index");
+    }
+  }
+}
