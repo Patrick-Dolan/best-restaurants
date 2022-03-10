06a89d7c5a1fff58807ecff8cf98516e0129fdbb
Patrick Dolan <dolanp1992@gmail.com>
2022-03-09 20:33:04 -0800

    add cuisine controller and model
    Co-authored-by: Branden Clauson <clauson.branden@gmail.com>
    Co-authored-by: Marni Sucher <suchermarni@gmail.com>
    Co-authored-by: Zoe Weinstein <zweinstein@gmail.com>

--- BestRestaurants/Models/BestRestaurantsContext.cs
+++ BestRestaurants/Models/BestRestaurantsContext.cs
@@ -5,7 +5,13 @@ namespace BestRestaurants.Models
   public class BestRestaurantsContext : DbContext
   {
     public DbSet<Restaurant> Restaurants { get; set; }
+    public DbSet<Cuisine> Cuisines { get; set; }
 
     public BestRestaurantsContext(DbContextOptions options) : base(options) { }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+      optionsBuilder.UseLazyLoadingProxies();
+    }
   }
 }
--- BestRestaurants/Models/Restaurant.cs
+++ BestRestaurants/Models/Restaurant.cs
@@ -5,5 +5,6 @@ namespace BestRestaurants.Models
     public int RestaurantId { get; set; }
     public string Name { get; set; }
     public string Description { get; set; }
+    public virtual Cuisine Cuisine { get; set; }
   }
 }   
